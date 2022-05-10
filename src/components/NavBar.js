0dab36f88c13b608baef0d376e1a6cdfe06e733e
ryanadams <rcoleman.adams@gmail.com>
2022-05-10 16:10:18 -0500

    navBarandFormattingChanges

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import Home from './Home';
-import Game from './Game';
-import Leaderboard from './Leaderboard';
+
 
 export default function NavBar() {
+  const styleBar = {
+    color: "white",
+    font: "Droid Sans",
+    background: "black",
+  }
+
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/game">Start A Game</NavLink>
-      <NavLink to="/leaderboard">Leaderboard</NavLink>
+    <div className='navbar'>
+      <NavLink 
+      style={({isActive})=>({
+        color: isActive ? "#FFE81F":"white"
+      })}
+      to="/"
+      >Home</NavLink>
+      <NavLink 
+      style={({isActive})=>({
+        color: isActive ? "#FFE81F":"white"
+      })}
+      to="/game"
+      >Start A Game</NavLink>
+      <NavLink 
+      style={({isActive})=>({
+        color: isActive ? "#FFE81F":"white"
+      })}
+      to="/leaderboard"
+      >Leaderboard</NavLink>
     </div>
   )
 }
