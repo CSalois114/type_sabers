7ac379d32fa35821bfe450991fe2ca4536488e47
Chris Salois <csalois@gmail.com>
2022-05-09 22:03:31 -0400

    basic game added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import React from "react"
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Leaderboard from "./Leaderboard";
+import Game from "./Game";
+import MiniLeaderboard from "./MiniLeaderboard";
+
 export default function App() {
   return (
     <div>
-      Typing Game or whatever
+      <NavBar />
+      <Game />
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function Game() {
+  const [gameText, setGameText] = useState("");
+  const [userEntry, setUserEntry] = useState("");
+  const [textIndex, setTextIndex] = useState(0);
+  console.log(textIndex, gameText.charAt(textIndex))
+
+  const completed = gameText.slice(0, textIndex);
+  const uncompleted = gameText.slice(textIndex);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/gameTexts")
+      .then((res) => res.json())
+      .then((data) => setGameText(data.level1));
+  }, []);
+
+  const handleEntry = (e) => {
+    if(e.target.value.slice(-1) === gameText.charAt(textIndex)){
+      setUserEntry(gameText.charAt(textIndex) === " " ? "" : e.target.value)
+      setTextIndex(textIndex + 1)  
+    }
+  }
+
   return (
-    <div>Game</div>
-  )
+    <div id="game">
+      <p>
+        <span className="completed">{completed}</span>
+        <span className="uncompleted">{uncompleted}</span>
+      </p>
+      <input onChange={handleEntry} value={userEntry} />
+    </div>
+  );
 }
