d0615884cbe566033f06f5b301da08e8a3346d53
ryanadams <rcoleman.adams@gmail.com>
2022-05-10 14:51:31 -0500

    basicRouteStructure

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import Home from './Home';
+import Game from './Game';
+import Leaderboard from './Leaderboard';
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/game">Start A Game</NavLink>
+      <NavLink to="/leaderboard">Leaderboard</NavLink>
+    </div>
   )
 }
