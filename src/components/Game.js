5e5be9325d36d0b2ea9406eace3728cf269cc855
Chris Salois <csalois@gmail.com>
2022-05-10 13:56:56 -0400

    add some styling to gamebox and reformat gametext storage

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from "react";
 
 export default function Game() {
-  const [gameText, setGameText] = useState("");
+  const [level, setLevel] = useState(0)
+  const [gameObj, setGameObj] = useState({
+    level: "",
+    title: "",
+    text: ""
+  });
   const [userEntry, setUserEntry] = useState("");
   const [textIndex, setTextIndex] = useState(0);
   const [errors, setErrors] = useState(0);
-  console.log(textIndex, gameText.charAt(textIndex))
+  console.log(textIndex, gameObj.text.charAt(textIndex))
 
-  const completed = gameText.slice(0, textIndex);
-  const uncompleted = gameText.slice(textIndex);
+  const completed = gameObj.text.slice(0, textIndex);
+  const uncompleted = gameObj.text.slice(textIndex);
 
   useEffect(() => {
     fetch("http://localhost:8001/gameTexts")
       .then((res) => res.json())
-      .then((data) => setGameText(data.level1));
+      .then((data) => setGameObj(data[level]));
   }, []);
 
   const handleEntry = (e) => {
-    if(e.target.value.slice(-1) === gameText.charAt(textIndex)){
-      setUserEntry(gameText.charAt(textIndex) === " " ? "" : e.target.value)
+    if(e.target.value.slice(-1) === gameObj.text.charAt(textIndex)){
+      setUserEntry(gameObj.text.charAt(textIndex) === " " ? "" : e.target.value)
       setTextIndex(textIndex + 1)  
     } else {
       setErrors(errors + 1)
@@ -29,6 +34,8 @@ export default function Game() {
     <div id="game">
       <div id="gameTextBox">
         <div id="gameText">
+          <h3 className="completed title">{gameObj.level}</h3>
+          <h2 className="completed title">{gameObj.title}</h2>
           <span className="completed">{completed}</span>
           <span className="uncompleted">{uncompleted}
             <div id="cursor">|</div>
