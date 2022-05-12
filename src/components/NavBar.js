15f2f9cf79389f88810adfd1ee2abb4ce44bd0dc
ryanadams <rcoleman.adams@gmail.com>
2022-05-12 09:12:31 -0500

    Cursor Change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,25 +3,23 @@ import { NavLink } from 'react-router-dom';
 
 
 export default function NavBar() {
+  const navLinkStyle = ({isActive})=>({
+    color: isActive ? "#FFE81F":"white",
+    cursor: "url(https://cdn.custom-cursor.com/db/9763/32/star-wars-millennium-falcon-pointer.png), auto",
+  })
 
   return (
     <div id="swFont" className='navbar'>
       <NavLink 
-      style={({isActive})=>({
-        color: isActive ? "#FFE81F":"white"
-      })}
+      style={navLinkStyle}
       to="/"
       >Home</NavLink>
       <NavLink 
-      style={({isActive})=>({
-        color: isActive ? "#FFE81F":"white",
-      })}
+      style={navLinkStyle}
       to="/episodes"
       >Episodes</NavLink>
       <NavLink 
-      style={({isActive})=>({
-        color: isActive ? "#FFE81F":"white"
-      })}
+      style={navLinkStyle}
       to="/leaderboard"
       >Jedi Masters</NavLink>
     </div>
